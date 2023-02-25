3f12a0a244fc7f78ce6544a2ff95327672f8a590
sblanco20141 <79016820+sblanco20141@users.noreply.github.com>
2023-02-25 14:00:59 -0600

    Cambio

--- EjemploGIT/src/ejemplogit/main.java
+++ EjemploGIT/src/ejemplogit/main.java
@@ -16,6 +16,7 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         int i = 0; //Cambio por Steven Blanco
+        int h =0;
     }
     /*Hola Cambio por Marco*/
 }
